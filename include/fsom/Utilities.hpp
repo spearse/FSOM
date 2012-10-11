21539af7eba40d2c2a7a78708d1cadbe9bc16b95
David Devaney <d_devaney@hotmail.co.uk>
2012-10-11 22:23:03 +0100

    further mods to remap function

--- include/fsom/Utilities.hpp
+++ include/fsom/Utilities.hpp
@@ -104,9 +104,9 @@ inline float remap(float r1_a, float r1_b, float r2_a, float r2_b, float in){
   float d1 = r1_b - r1_a;
   float d2 = r2_b - r2_a;
   
-  float x = in / d1;
+ // float x = in / d1;
   
-  return x * d2;
+  return (in - r1_a) * d2 / d1 + r2_a;
 }
 
 /// a simple dsp phasor class for use in oscilator and control objects.
