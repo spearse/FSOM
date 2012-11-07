a7aa6722ebe90be0db0ec6f413dd2c0f5a969061
stephen <stephen@stephen-VirtualBox.(none)>
2012-11-07 18:32:06 +0000

    tweak to loop logic

--- src/Session.cpp
+++ src/Session.cpp
@@ -484,11 +484,11 @@ void Session::process(float** ins,float** outs,int frameCount,int channelCount){
 		float sr = Engine::get_instance().get_sample_rate();
 		//std::cout << "Transport rolling: t="<< m_playHead << " " << float(m_playHead)/sr << std::endl;
 		//simple idea for looping
-		if(m_loopState &&  m_playHead >= m_rightLocator){
-		    /*
+		if(m_loopState &&  m_playHead <= m_leftLocator){
+		    
 		    m_playHead = m_leftLocator;
 		    seek(m_leftLocator);
-		    */
+		    
 		}
 		SamplePosition endOfBlockTime = m_playHead + frameCount;
 		
